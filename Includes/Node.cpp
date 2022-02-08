f245ca5b800990502799561ad8e870baa84f2d6b
devpro13 <076bei013.devraj@pcampus.edu.np>
2022-02-08 11:31:06 +0545

    Node.h and Node.cpp files updated..

--- Includes/Node.cpp
+++ Includes/Node.cpp
@@ -1,1 +1,41 @@
- 
+#inlclude"Node.h"
+Queue Q;
+NodeTree::NodeTree(){
+	root=NULL;
+}
+static void NodeTree::incID(){
+	if(nodeID!=NULL){
+		*nodeID+=1;
+	}
+	else{
+		nodeID=(int*)malloc(sizeof(int));
+		*nodeID=0;
+	}
+}		
+void NodeTree::createNode(double xpos,double ypos){
+	incID();
+	
+	if(root==NULL){
+		root=(Node*)malloc(sizeof(Node));
+		root->xpos=xpos;
+		root->ypos=ypos;
+		root->id=nodeID;
+		Q.Enqueue(root);
+	}
+	else{
+		Node*newNode=(Node*)malloc(sizeof(Node));
+		newNode->xpos=xpos;
+		newNode->ypos=ypos;
+		newNode->id=nodeID;
+		Q.Enqueue(newNode);
+	}
+}
+void joinNode(Node* node1,Node* node2){
+	node1->Edges->push_back(node2);
+	node2->Edges->push_back(node1);
+}
+void flagSrcDest(Node*node1,Node*node2){
+	node1->flagSrc=true;
+	node2->flagDest=true;
+}
+	
--- Includes/Node.h
+++ Includes/Node.h
@@ -1,23 +1,24 @@
 #ifndef NODE_H
 #define NODE_H
 #include<vector>
+#include"Queue.h"
 class Node{
 	public:
 	vector<Node*>Edges;//stores edges
-	bool flagSrc;//flag node is destination
-	bool flagDest;//flag node is source
+	bool flagSrc=false;//flag node is destination
+	bool flagDest=false;//flag node is source
 	double xpos,ypos;//node position
 	int id;//node id or vertex number
 	double weight;
 };
 class NodeTree{
 	Node* root;
-	double xpos,ypos;
-	static int nodeID;
+	static int *nodeID;
 	public:
-	NodeTree(double,double);
-	void createNode();
-	void joinNode();
-	void flagSrcDest();
+	NodeTree();
+	void createNode(double,double);
+	void joinNode(Node*,Node*);
+	void flagSrcDest(Node*,Node*);
+	static void incID();
 };
 #endif
