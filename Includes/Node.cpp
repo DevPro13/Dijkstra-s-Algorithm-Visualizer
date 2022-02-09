eaedd9dab3bdf1c913d02c23dbe2d2361914e5fe
devpro13 <076bei013.devraj@pcampus.edu.np>
2022-02-09 13:08:32 +0545

    Queue.h and Queue.cpp removed as their source code merged in Node.h

--- Includes/Node.cpp
+++ Includes/Node.cpp
@@ -1,5 +1,4 @@
 #inlclude"Node.h"
-Queue Q;
 NodeTree::NodeTree(){
 	root=NULL;
 }
@@ -12,7 +11,7 @@ static void NodeTree::incID(){
 		*nodeID=0;
 	}
 }		
-void NodeTree::createNode(double xpos,double ypos){
+void NodeTree::createNode(Queue Q,double xpos,double ypos){
 	incID();
 	
 	if(root==NULL){
@@ -38,4 +37,44 @@ void NodeTree::flagSrcDest(Node*node1,Node*node2){
 	node1->flagSrc=true;
 	node2->flagDest=true;
 }
-	
+Queue::Queue():head(NULL){}
+void Queue::Enqueue(Node*treeNodeAddress){
+	if(head==NULL){
+		head=(node*)malloc(sizeof(node));
+		head->newTreeNode=treeNodeAddress;
+		head->next=NULL;
+	}
+	else{
+		node* tmp=head;
+		node*newNode=(node*)malloc(sizeof(node));
+		while(tmp!=NULL)tmp=tmp->next;
+		newNode->next=NULL;
+		newNode->newTreeNode=treeNodeAddress;
+		tmp->next=newNode;
+	}
+}
+void Queue::Delete(){
+	if(head==NULL)
+	{
+		std::cout<<"Queue is empty"<<std::endl;
+	}
+	node*tmpHead=head;
+	while(tmpHead->next!=NULL){
+		delete(head->treeNodeAddress);
+		tmpHead->tmpHead->next;
+		delete(head);
+		head=tmpHead;
+	}
+	std::cout<<"All Nodes successfully Deleted"<<std::endl;
+}
+Node* Queue::Dequeue(int key){
+	node*tmpHead=head;
+	while(tmpHead!=NULL){
+		if(tmpHead->treeNodeAddress->id==key){
+			return tmp->treeNodeAddress;
+		}
+		tmpHead=tmpHead->next;
+	}
+	return NULL;
+}
+
--- Includes/Node.h
+++ Includes/Node.h
@@ -1,7 +1,12 @@
 #ifndef NODE_H
 #define NODE_H
+#include<iostream>
+#include<stdlib.h>
 #include<vector>
-#include"Queue.h"
+class Node;
+class node;
+class NodeTree;
+class Queue;
 class Node{
 	public:
 	vector<Node*>Edges;//stores edges
@@ -9,16 +14,31 @@ class Node{
 	bool flagDest=false;//flag node is source
 	double xpos,ypos;//node position
 	int id;//node id or vertex number
-	double weight;
 };
-class NodeTree:public class Queue{
+class NodeTree{
 	Node* root;
 	static int *nodeID;
 	public:
 	NodeTree();
-	void createNode(double,double);
+	void createNode(Queue,double,double);
 	void joinNode(Node*,Node*);
 	void flagSrcDest(Node*,Node*);
 	static void incID();
 };
+//Queue implementation using linked list
+class node{
+	public:
+	Node* newTreeNode;//for Node address
+	node* next;//for next node address in linked list
+};
+class Queue{
+	node *head;//for head node
+	public:
+	Queue();
+	void Enqueue(Node*);
+	Node*Dequeue(int key);//for dequeueAll node address
+	void Delete();//delete all Node and nodes in Linked list
+};
+
+
 #endif
